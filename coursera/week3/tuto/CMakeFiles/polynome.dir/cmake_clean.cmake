cf3c2cbdc93211dcb66473bf754ab2cfcf29fd93
Abdoulaye Diallo <vonewman7@gmail.com>
2019-09-14 23:24:54 +0000

    cmake stuff

---
+++ coursera/week3/tuto/CMakeFiles/polynome.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/polynome.dir/polynome.cpp.o"
+  "polynome.pdb"
+  "polynome"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/polynome.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
